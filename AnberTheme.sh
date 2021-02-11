d5bb3aee820dc5bb5bb9df14321794185e73b2fb
TadMSTR <69825253+TadMSTR@users.noreply.github.com>
2021-02-11 08:28:45 -0500

    Delete AnberTheme.sh

--- AnberTheme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Notes
-#
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/AnberTheme"
-rm -rf /tmp/AnberTheme.log
-bash AnberTheme 2>&1 | tee -a /tmp/AnberTheme.log
